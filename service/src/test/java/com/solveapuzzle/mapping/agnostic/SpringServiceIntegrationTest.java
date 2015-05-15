94b3505a96efc63464ba876cfa769260a48934e2
Neil Piper <neil.piper@iag.com.au>
2015-05-16 08:45:57 +1000

    Updated configuration & added working Spring integration test

--- service/src/test/java/com/solveapuzzle/mapping/agnostic/SpringServiceIntegrationTest.java
+++ service/src/test/java/com/solveapuzzle/mapping/agnostic/SpringServiceIntegrationTest.java
@@ -3,6 +3,9 @@ package com.solveapuzzle.mapping.agnostic;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.nio.charset.Charset;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +16,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration
 public class SpringServiceIntegrationTest {
 
+	@Autowired
+	private MappingEngine mappingEngine;
+	
+	String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+"<message>Yep, it worked!</message>";
+	
 	@Test
-	public void testOne()
+	public void testOne() throws MappingException, ConfigurationException
 	{
+		MappingConfiguration config = new MappingConfiguration() {
+			
+			public String getMappingType() {
+				// TODO Auto-generated method stub
+				return "saxonMapper";
+			}
+			
+			public String getMappingIdentifer() {
+				// TODO Auto-generated method stub
+				return "test";
+			}
+		};
 		
+		String output = mappingEngine.onTransformEvent(config, xml, Charset.defaultCharset());
+		
+		Assert.assertFalse(output.isEmpty());
+		Assert.assertEquals("Yep, it worked!", output);
 	}
 	
 }
